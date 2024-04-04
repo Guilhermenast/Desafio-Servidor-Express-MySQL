242251706b1e6a8b18136c75fd7e8eab0759887f
Guilhermenast <guilhermenast2021@gmail.com>
2024-04-03 21:47:47 -0300

    Ajustes

--- routes/clientes.js
+++ routes/clientes.js
@@ -3,12 +3,18 @@ const router = express.Router();
 
 const clienteController = require('../controllers/clienteController');
 const nomeMiddleware = require('../middlewares/nomeMiddleware');
+const sobrenomeMiddleware = require('../middlewares/sobrenomeMiddleware');
+const idadeMiddleware = require('../middlewares/idadeMiddleware');
 
 /* GET clientes */
 router.get('/', clienteController.findAll);
 
 /* POST clientes */
-router.post('/', nomeMiddleware.validaNome, clienteController.save);
+router.post('/', nomeMiddleware.validaNome,
+ sobrenomeMiddleware.validateFamilyName,
+ idadeMiddleware.validateAge,
+ clienteController.save
+);
 
 /* PUT clientes */
 router.put('/', clienteController.update);
